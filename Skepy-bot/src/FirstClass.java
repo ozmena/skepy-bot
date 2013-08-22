25994389e78b61f91f05ca6b430e84d29dbfd674
ozmena <ozmenad@gmail.com>
2013-08-22 17:06:07 +0300

    third one

--- Skepy-bot/src/FirstClass.java
+++ Skepy-bot/src/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("hello git hub");
 		System.out.println("git come on!");
+		System.out.println("third one");
 		
 	}
 	
